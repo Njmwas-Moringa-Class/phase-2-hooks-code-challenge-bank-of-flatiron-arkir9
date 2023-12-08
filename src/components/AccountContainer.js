4f8461ec2533a4c1aa4ab73aa061af216b24010a
Mohammed <khamismitch@gmail.com>
2023-12-08 10:52:32 +0300

    complete

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const response = await fetch("http://localhost:8001/transactions");
+        const data = await response.json();
+        setTransactions(data);
+        setFilteredTransactions(data);
+      } catch (error) {
+        console.error("Error fetching transactions:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTransactions();
+  }, []);
+
+  const handleAddTransaction = (newTransaction) => {
+    setTransactions([...transactions, { id: Date.now(), ...newTransaction }]);
+  };
+
+  const handleSearch = (searchTerm) => {
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    const filtered = transactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(lowerCaseSearchTerm)
+    );
+    setFilteredTransactions(filtered);
+  };
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search onSearch={handleSearch} />
+      <AddTransactionForm onAddTransaction={handleAddTransaction} />
+      {loading ? (
+        <p>Loading transactions...</p>
+      ) : (
+        <TransactionsList transactions={filteredTransactions} />
+      )}
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Search() {
+function Search({ onSearch }) {
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        onChange={(e) => onSearch(e.target.value)}
       />
       <i className="circular search link icon"></i>
     </div>
